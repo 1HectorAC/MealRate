39e621d9c085076a78c3312fcde512494aead04e
Hector Acosta <1Hector.AC@gmail.com>
2025-08-07 14:08:00 -0700

    Added some backend stuff.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
 const express = require('express');
-//const { GoogleGenerativeAI } = require('@google-generative-ai'); 
+const { GoogleGenerativeAI } = require('@google-generative-ai'); 
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 
+const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 app.get('/', (req,res) => {
 	res.send('Test');
+})
+
+app.post('/api/generate', async (req,res) =>{
+	try{
+		const { prompt } = req.body;
+		if(!prompt)
+			return res.status(400).send({error:"Prompt Required"});
+		const model = genAi.getGenerativeModel({model:'gemini-2.5-flash'});
+		const result = await model.generateContent(prompt);
+		const response = result.response;
+		const text = response.text();
+		res.send({generated_text:text});
+	}
+	catch (error){
+		console.log('error ', error);
+		res.status(500).send({error:'Failure to generate content.'});
+	}
 })
 
 app.listen(5000, () => {console.log("Listining on Port 5000")});
